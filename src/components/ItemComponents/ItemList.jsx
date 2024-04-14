161b56268d509db997439b4d35ee0ce563a985e7
leandro-cajal <leo_origin@hotmail.com>
2024-04-14 15:11:26 -0300

    checkout page added

--- src/components/ItemComponents/ItemList.jsx
+++ src/components/ItemComponents/ItemList.jsx
@@ -4,7 +4,7 @@ const ItemList = ({products}) =>{
     return(
         <div className='w-full py-28'>
                 <div className='max-w-7xl mx-auto'>
-                    <div className='grid grid-cols-1 min-[400px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:justify-center justify-items-center px-4 xl:px-0'>
+                    <div className='grid grid-cols-1 pt-10 lg:pt-0 md:grid-cols-3 lg:grid-cols-4 gap-4 md:justify-center justify-items-center px-4 xl:px-0'>
                         {products.length > 0 &&
                             products.map((product) => {
                                 return (
