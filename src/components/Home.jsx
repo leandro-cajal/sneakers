52e4cd367ba24e2be722ffefbf45a84d8c8a3e45
leandro-cajal <leo_origin@hotmail.com>
2024-04-15 05:26:00 -0300

    checkout page added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,11 @@ const Home = () => {
     return (
         <section className='pt-32 pb-20 space-y-16'>
             <div className='mx-auto max-w-7xl space-y-14 pb-4'>
-                <div className='flex-col items-center'>
-                    <img className="w-full" src="https://nikearprod.vtexassets.com/assets/vtex.file-manager-graphql/images/9740da8c-2d64-4882-a372-5087e27b4e38___748441f706e566e86c779225abc3e0de.webp" alt="" />
-                </div>
+                <Link to="/zapatillas/new">
+                    <div className='flex-col items-center'>
+                        <img className="w-full" src="https://nikearprod.vtexassets.com/assets/vtex.file-manager-graphql/images/9740da8c-2d64-4882-a372-5087e27b4e38___748441f706e566e86c779225abc3e0de.webp" alt="" />
+                    </div>
+                </Link>
                 <div className="flex flex-col items-center gap-4">
                     <p className="text-3xl md:text-[50px] font-black">RUN YOUR RUN</p>
                     <Link className="bg-black rounded-full text-white px-6 py-3 font-bold text-sm hover:bg-red-600 transition-all duration-300" to={`/zapatillas/new`}>Comprar</Link>
